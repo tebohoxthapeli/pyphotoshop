db814737da14195fa9a5cab556c0d518ac693699
Teboho Thapeli <tm.thapeli@gmail.com>
2022-09-30 23:35:10 +0200

    added more comments and changed a couple of variable names for clarity

--- image.py
+++ image.py
@@ -1,21 +1,44 @@
-import numpy as np
-import png
+from numpy import zeros, clip, vstack
+from png import Reader, Writer
 
 
 class Image:
-    def __init__(self, x_pixels=0, y_pixels=0, num_channels=0, filename=""):
+    # def __init__(self, x_pixels=0, y_pixels=0, num_channels=0, filename=""):
+    #     # you need to input either filename OR x_pixels, y_pixels, and num_channels
+    #     self.input_path = "input/"
+    #     self.output_path = "output/"
+
+    #     if x_pixels and y_pixels and num_channels:
+    #         self.x_pixels = x_pixels
+    #         self.y_pixels = y_pixels
+    #         self.num_channels = num_channels
+    #         self.array = zeros((x_pixels, y_pixels, num_channels))
+    #     elif filename:
+    #         self.array = self.read_image(filename)
+    #         self.x_pixels, self.y_pixels, self.num_channels = self.array.shape
+    #     else:
+    #         raise ValueError(
+    #             "You need to input either a filename OR specify the dimensions of the image"
+    #         )
+
+    def __init__(
+        self, shape: tuple[int, int, int] | None = None, filename: str | None = None
+    ):
         # you need to input either filename OR x_pixels, y_pixels, and num_channels
+
         self.input_path = "input/"
         self.output_path = "output/"
 
-        if x_pixels and y_pixels and num_channels:
-            self.x_pixels = x_pixels
-            self.y_pixels = y_pixels
-            self.num_channels = num_channels
-            self.array = np.zeros((x_pixels, y_pixels, num_channels))
+        if shape:
+            self.x_pixels = shape[0]
+            self.y_pixels = shape[1]
+            self.num_channels = shape[2]
+            self.array = zeros(shape)
+
         elif filename:
             self.array = self.read_image(filename)
             self.x_pixels, self.y_pixels, self.num_channels = self.array.shape
+
         else:
             raise ValueError(
                 "You need to input either a filename OR specify the dimensions of the image"
@@ -26,8 +49,8 @@ class Image:
         read PNG RGB image, return 3D numpy array organized along Y, X, channel
         values are float, gamma is decoded
         """
-        im = png.Reader(self.input_path + filename).asFloat()
-        resized_image = np.vstack(list(im[2]))
+        im = Reader(self.input_path + filename).asFloat()
+        resized_image = vstack(list(im[2]))
         resized_image.resize(im[1], im[0], 3)
         resized_image = resized_image**gamma
         return resized_image
@@ -36,10 +59,10 @@ class Image:
         """
         3D numpy array (Y, X, channel) of values between 0 and 1 -> write to png
         """
-        im = np.clip(self.array, 0, 1)
+        im = clip(self.array, 0, 1)
         y, x = self.array.shape[0], self.array.shape[1]
         im = im.reshape(y, x * 3)
-        writer = png.Writer(x, y)
+        writer = Writer(x, y)
         with open(self.output_path + output_file_name, "wb") as f:
             writer.write(f, 255 * (im ** (1 / gamma)))
 
--- transform_empty.py
+++ transform_empty.py
@@ -2,74 +2,78 @@ from image import Image
 import numpy as np
 
 
-def get_new_image(old_img_arr_shape: tuple[int, ...]) -> Image:
-    x_pixel, y_pixel, num_channels = old_img_arr_shape
-    return Image(x_pixel, y_pixel, num_channels)
-
-
-def brighten(image: Image, factor: float) -> Image:
+def brighten(image: Image, factor=2.0) -> Image:
     # when we brighten, we just want to make each channel higher by some amount
     # factor is a value > 0, how much you want to brighten the image by (< 1 = darken, > 1 = brighten)
 
     # make empty image so we don't modify original one
-    new_im = get_new_image(image.array.shape)
+    new_img = Image(image.array.shape)
 
-    # this is the most intuitive way to do this (non-vectorised)
+    # non-vectorised way (easier to understand):
+    # brighten image by multiplying each pixel by the factor
+
     # for x in range(x_pixels):
     #     for y in range(y_pixels):
     #         for c in range(num_channels):
-    #             new_im.array[x, y, c] = image.array[x, y, c] * factor
+    #             new_img.array[x, y, c] = image.array[x, y, c] * factor
 
-    # vectorised version which leverages numpy (does the same as the above, but faster)
-    new_im.array = image.array * factor
+    # vectorised version which leverages numpy (does the same as the above, but faster):
+    new_img.array = image.array * factor
 
-    return new_im
+    return new_img
 
 
-def adjust_contrast(image: Image, factor: float, mid: float = 0.5) -> Image:
+def adjust_contrast(image: Image, factor: float, mid=0.5) -> Image:
     # adjust the contrast by increasing the difference from the user-defined midpoint by
     # factor amount
-    new_im = get_new_image(image.array.shape)
 
-    for x in range(new_im.x_pixels):
-        for y in range(new_im.y_pixels):
-            for c in range(new_im.num_channels):
-                new_im.array[x, y, c] = (image.array[x, y, c] - mid) * factor + mid
+    new_img = Image(image.array.shape)
 
-    # vectorised
-    # new_im.array = (image.array - mid) * factor + mid
+    # for x in range(new_img.x_pixels):
+    #     for y in range(new_img.y_pixels):
+    #         for c in range(new_img.num_channels):
+    #             new_img.array[x, y, c] = ((image.array[x, y, c] - mid) * factor) + mid
 
-    return new_im
+    # vectorised:
+    new_img.array = ((image.array - mid) * factor) + mid
+
+    return new_img
 
 
 def blur(image: Image, kernel_size: int) -> Image:
+    # we blur by averaging a pixel with its neighbours (which will be determined by the kernel)
     # kernel size is the number of pixels to take into account when applying the blur
-    # (ie kernel_size = 3 would be neighbors to the left/right, top/bottom, and diagonals)
+    # (ie kernel_size = 3 would be kernel_size / 2 neighbours to the left/right, top/bottom,
+    # and diagonals)
     # kernel size should always be an *odd* number
 
-    new_im = get_new_image(image.array.shape)
+    new_img = Image(image.array.shape)
 
     # this is a variable that tells us how many neighbours we actually look at (ie for a kernel of
     # 3, this value should be 1)
+    # how many neighbours to one side we need to look at
     neighbour_range = kernel_size // 2
 
-    for x in range(new_im.x_pixels):
-        for y in range(new_im.y_pixels):
-            for c in range(new_im.num_channels):
+    for x in range(new_img.x_pixels):
+        for y in range(new_img.y_pixels):
+            for c in range(new_img.num_channels):
                 total = 0
 
                 for x_i in range(
                     max(0, x - neighbour_range),
-                    min(new_im.x_pixels - 1, x + neighbour_range) + 1,
+                    min(new_img.x_pixels - 1, x + neighbour_range) + 1,
                 ):
                     for y_i in range(
                         max(0, y - neighbour_range),
-                        min(new_im.y_pixels - 1, y + neighbour_range) + 1,
+                        min(new_img.y_pixels - 1, y + neighbour_range) + 1,
                     ):
                         total += image.array[x_i, y_i, c]
-                new_im.array[x, y, c] = total / (kernel_size**2)
 
-    return new_im
+                # kernel_size**2 because the neighbours create a perfect square
+                # this is the total number of neighbours to consider
+                new_img.array[x, y, c] = total / (kernel_size**2)
+
+    return new_img
 
     # note:
     # the blur implemented above is a kernel of size n, where each value is 1/n^2
@@ -87,7 +91,7 @@ def apply_kernel(image: Image, kernel: np.ndarray) -> Image:
     # [2 0 -2]
     # [1 0 -1]
 
-    new_im = get_new_image(image.array.shape)
+    new_im = Image(image.array.shape)
     kernel_size = kernel.shape[0]
     neighbour_range = kernel_size // 2
 
@@ -117,7 +121,7 @@ def combine_images(image1: Image, image2: Image) -> Image:
     # let's combine two images using the squared sum of squares: value = sqrt(value_1**2, value_2**2)
     # size of image1 and image2 MUST be the same
 
-    new_im = get_new_image(image1.array.shape)
+    new_im = Image(image1.array.shape)
 
     for x in range(new_im.x_pixels):
         for y in range(new_im.y_pixels):
@@ -129,20 +133,22 @@ def combine_images(image1: Image, image2: Image) -> Image:
 
 
 if __name__ == "__main__":
+    print("Processing image. Hold on...")
+
     lake = Image(filename="lake.png")
     city = Image(filename="city.png")
 
-    # brightened_im = brighten(lake, 2.0)
-    # brightened_im.write_image("brightened_image.png")
+    # brightened image
+    # brighten(lake).write_image("bright_lake.png")
 
-    # reduced brightness
-    # brighten(lake, 0.5).write_image("reduced_brightness_lake.png")
+    # reduce brightness (darken)
+    # brighten(lake, 0.5).write_image("dark_lake.png")
 
     # increase contrast
     # adjust_contrast(lake, 2.0).write_image("increased_contrast_lake.png")
 
     # decrease contrast
-    # adjust_contrast(lake, 0.5).write_image("decreased_contrast_lake.png")
+    adjust_contrast(lake, 0.5).write_image("decreased_contrast_lake.png")
 
     # blurred with kernel of 3
     # blur(lake, 3).write_image("blurred_lake_3.png")
@@ -171,8 +177,10 @@ if __name__ == "__main__":
     # apply_kernel(city, sobel_x_kernel).write_image("edge_x.png")
     # apply_kernel(city, sobel_y_kernel).write_image("edge_y.png")
 
-    combine_images(
-        Image(filename="edge_x.png"), Image(filename="edge_y.png")
-    ).write_image("edge_xy.png")
+    # combine_images(
+    #     Image(filename="edge_x.png"), Image(filename="edge_y.png")
+    # ).write_image("edge_xy.png")
 
-    print("Done processing image.")
+    print(
+        f"Done processing image. Find the new image in the {lake.output_path} folder."
+    )
